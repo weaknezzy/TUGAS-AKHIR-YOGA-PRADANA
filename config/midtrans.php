e4ea1fd54371f422ae16c0443245058e0c89deb8
Yoga Pradana <132411206+weaknezzy@users.noreply.github.com>
2025-09-30 10:49:06 +0700

    perbersihan repository

--- config/midtrans.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'server_key' => env('MIDTRANS_SERVER_KEY'),
-    'client_key' => env('MIDTRANS_CLIENT_KEY'),
-    'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
-];
--- resources/views/filament/resources/laporan-resource/note-modal.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="p-4 space-y-4">
-    <div class="prose max-w-none">
-        {{ $note }}
-    </div>
-</div> 
